ad97c1ed9176b2817631d71090d2723541b1247b
ancjf <ancjf@163.com>
2018-04-28 11:24:09 +0800

    添加springboot模块

---
+++ api/src/main/java/org/web3j/boottest.java
@@ -0,0 +1,41 @@
+package org.web3j;
+
+/**
+ * Created by tangjc on 2018/4/27.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.methods.response.Web3ClientVersion;
+
+import java.io.IOException;
+
+@Controller
+@EnableAutoConfiguration
+
+public class boottest {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        Web3j web3 = Web3j.build(new org.web3j.protocol.http.HttpService("http://192.168.1.189:8545/"));  // defaults to http://localhost:8545/
+    Web3ClientVersion web3ClientVersion = null;
+        try
+    {
+        web3ClientVersion = web3.web3ClientVersion().send();
+    } catch (IOException e)
+    {
+        e.printStackTrace();
+    }
+    String clientVersion = web3ClientVersion.getWeb3ClientVersion();
+        return clientVersion;
+}
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(boottest.class, args);
+    }
+}
